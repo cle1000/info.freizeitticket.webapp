98d38590a96315b4b4c2d2f0916d8f275824fd0d
Clemens <cle1000.cb@gmail.com>
2016-10-20 10:36:45 +0200

    app storage

--- app/assets/javascripts/components/InfoBar.js.jsx
+++ app/assets/javascripts/components/InfoBar.js.jsx
@@ -1,10 +1,13 @@
 function InfoBar ( {} ){
-	showAppAdds = !document.cookie.includes("app")
+	showAppAdds = !localStorage.getItem("app");
 
     return (
     		<div className="col-xs-12">
+    			<a data-toggle="modal" href="#infoModal"><img alt="Information" className="info-img pull-left" src="/images/info.svg"/></a>
+				<a data-toggle="modal" href="#avalancheModal"><img alt="Lawinen Information" className="info-img pull-left" src="/images/avalancheinfo.png"/></a>
     			{showAppAdds && (
     				<div className="">
+
 						<a target="_blank" href="https://play.google.com/store/apps/details?id=brunnerclemens.freizeitticketinfo">
 							<img  alt="play store"  className="appxs visible-xs" src="/images/app_icons/google_play.png"/>
 							<img  alt="play store"  className="app hidden-xs" src="/images/app_icons/google_play.png"/>
