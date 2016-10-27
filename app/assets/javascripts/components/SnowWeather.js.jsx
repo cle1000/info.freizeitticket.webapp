b28492c6153a34b479f746d42a08f964ab681e42
Clemens Wolfgang Brunner <clemens.brunner@student.uibk.ac.at>
2016-10-27 11:40:31 +0200

    add ssl proxy

--- app/assets/javascripts/components/SnowWeather.js.jsx
+++ app/assets/javascripts/components/SnowWeather.js.jsx
@@ -9,28 +9,13 @@ function SnowWeather ( { src } ){
 		    		width="250" 
 		    		className="schneehoehen"
 		    		name="sh_frame" 
-		    		src="/test.html" 
+		    		src={"https://ssl-proxy.my-addr.org/myaddrproxy.php/http/www.schneehoehen.de/widgets/details/widget_schneebericht/" + src + "?theme=blue&amp;language=de&amp;size=250&amp;weather=true"}
 		    		scrolling="no" 
 		    		id="sh_frame" 
 		    		frameBorder="0" 
 		    		height="410">
 		    	</iframe>
-
 		    </div>
 		</div>
     )
 }
-
-
-/*
-<iframe 
-		    		width="250" 
-		    		className="schneehoehen"
-		    		name="sh_frame" 
-		    		src={"http://www.schneehoehen.de/widgets/details/widget_schneebericht/"+ src + "?theme=grey&amp;language=de&amp;size=250&amp;weather=true"} 
-		    		scrolling="no" 
-		    		id="sh_frame" 
-		    		frameBorder="0" 
-		    		height="410">
-		    	</iframe>
-		    	*/
