65847e90605f8e4b021dd74dfc93451501e0f980
Clemens Wolfgang Brunner <clemens.brunner@student.uibk.ac.at>
2016-11-08 08:38:17 +0100

    change migration file

--- db/migrate/20161106074927_add_last_push_time_and_height_to_skiresort.rb
+++ db/migrate/20161106074927_add_last_push_time_and_height_to_skiresort.rb
@@ -1,6 +1,6 @@
 class AddLastPushTimeAndHeightToSkiresort < ActiveRecord::Migration
   def change
-  	add_column :Skiresorts, :last_push_time, :timestamp, :default => 1.year.ago
-  	add_column :Skiresorts, :last_push_height, :integer, :default => 0
+  	add_column :skiresorts, :last_push_time, :timestamp, :default => 1.year.ago
+  	add_column :skiresorts, :last_push_height, :integer, :default => 0
   end
 end
