06635075d823020192328b9808482d5d074bf31c
Clemens Wolfgang Brunner <clemens.brunner@student.uibk.ac.at>
2016-11-11 09:38:56 +0100

    add elements

---
+++ app/helpers/initialize_twitter.rb
@@ -0,0 +1,10 @@
+module InitializeTwitter
+	def init
+		$twitter = Twitter::REST::Client.new do |config|
+		  config.consumer_key = ENV['FREIZEITTICKET_TWITTER_CONSUMER_KEY']
+		  config.consumer_secret = ENV['FREIZEITTICKET_TWITTER_CONSUMER_SECRET']
+		  config.access_token = ENV['FREIZEITTICKET_TWITTER_ACCESS_TOKEN']
+		  config.access_token_secret = ENV['FREIZEITTICKET_TWITTER_ACCESS_SECRET']
+		end
+	end
+end
